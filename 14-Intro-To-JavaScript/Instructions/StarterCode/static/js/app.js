a608e240b0b5837b22c05576168b8d859e745b9f
smknight3 <71661810+smknight3@users.noreply.github.com>
2021-01-14 11:56:22 -0600

    Add

--- 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -1,40 +1,4 @@
-$(document).ready(function() { // from data.js
-            var tableData = data
-
-            var button = d3.select("#filter-btn");
-
-            button.on("click", runEnter);
-
-            function runEnter() {
-
-                // Prevent the page from refreshing
-                d3.event.preventDefault();
-
-                // Select the input element and get the raw HTML node
-                var inputElement = d3.select("#patient-form-input");
-
-                // Get the value property of the input element
-                var inputValue = inputElement.property("value");
-
-                console.log(inputValue);
-                console.log(people);
-
-                var filteredData = people.filter(person => person.bloodType === inputValue);
-
-                console.log(filteredData);
-
-
-                // YOUR CODE HERE!
-                var tbody = d3.select("#ufo-table>tbody");
-
-                console.log(data);
-
-                data.forEach(function(row) {
-                    console.log(row);
-
-                    var newRow = tbody.append("tr");
-                    Object.entries(row).forEach(function([key, value]) {
-                        newRow.append("td").text(value);
-                    });
-                });
-            });
+$(document).ready(function() {
+    var tableData = data;
+    console.log(tableData);
+});
