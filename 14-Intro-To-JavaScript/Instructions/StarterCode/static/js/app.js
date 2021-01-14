3c9eca1cf36f3a0e1b1ac8fe1045b1b8451d7508
smknight3 <71661810+smknight3@users.noreply.github.com>
2021-01-14 12:02:36 -0600

    Add:

--- 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,18 @@ $(document).ready(function() {
     var tableData = data;
     console.log(tableData);
 });
+
+function buildTable(data) {
+    var tbody = $("#myData > tbody");
+    tbody.empty();
+
+    data.forEach(function(row) {
+        var newRow = "<tr>"
+        Object.defineProperties(row).forEach(function([key, value]) {
+            newRow += `<td>${value}</td>`
+        });
+
+        newRow += "</tr>";
+        tbody.append(newRow);
+    });
+}
